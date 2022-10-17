66321970b24389771157bec85830e64d30e73571
Roberto Arcomano <bertolinux@gmail.com>
2022-10-17 17:29:43 +0200

    Moved secret from config to devops namespace

--- devops/start.sh
+++ devops/start.sh
@@ -20,12 +20,17 @@ kubectl replace -f $JENKINS_DEPLOY_FILE --force
 # 3. Create service jenkins
 kubectl replace -f $JENKINS_SVC_FILE --force
 
-# 4. Wait until pod is ready
+# 4. Create secret
+kubectl create -n $NS secret generic gitlab-registry-auth \
+  --from-file=.dockerconfigjson=$HOME/.docker/config.json \
+  --type=kubernetes.io/dockerconfigjson
+
+# 5. Wait until pod is ready
 JENKINS_POD=$(kubectl -n $NS get pod --no-headers | grep -i jenkins | awk '{print $1}')
 echo "JENKINS_POD: " $JENKINS_POD
 until kubectl -n $NS get pod $JENKINS_POD|grep Running; do sleep 1; done
 until kubectl -n $NS logs $JENKINS_POD |grep "Jenkins is fully up and running"; do sleep 1; done
 
-# 5. Show password
+# 6. Show password
 echo -e "Jenkins password: "
 kubectl -n $NS exec -it $JENKINS_POD -- cat $PASSWORD_FILE
