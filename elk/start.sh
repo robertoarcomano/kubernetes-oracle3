89752a9731dbd68589d36153eb084b8229697b10
Roberto Arcomano <info@robertoarcomano.com>
2022-10-29 20:47:07 +0200

    Added Elasticsearch as well as Kibana. WIP

---
+++ elk/start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+NS=elk
+NOW="--grace-period=0 --force"
+ELASTICSEARCH_DEPLOY_FILE=elasticsearch-deploy.yaml
+ELASTICSEARCH_SERVICE_FILE=elasticsearch-service.yaml
+KIBANA_DEPLOY_FILE=kibana-deploy.yaml
+KIBANA_SERVICE_FILE=kibana-service.yaml
+STARTUP_TIME=60
+
+#docker run --net elastic --name elasticsearch -p 9200:9200 -p 9300:9300 -it docker.elastic.co/elasticsearch/elasticsearch:8.4.3
+
+# 1. Create the namespace
+kubectl delete ns $NS $NOW
+kubectl create ns $NS
+
+# 2. Create deploys as well as services
+kubectl apply -f $ELASTICSEARCH_DEPLOY_FILE
+kubectl apply -f $ELASTICSEARCH_SERVICE_FILE
+kubectl apply -f $KIBANA_DEPLOY_FILE
+kubectl apply -f $KIBANA_SERVICE_FILE
+
+# 4. Get security data from elasticsearch
+# 4.1. Wait for the elasticsearch pod
+until kubectl -n $NS get pod --no-headers | grep elasticsearch | grep Running; do sleep 1; done
+# 4.2. Get pod name
+POD=$(kubectl -n $NS get pod --no-headers | grep elasticsearch | grep Running|awk '{print $1}')
+# 4.3. Wait for $STARTUP_TIME seconds
+sleep $STARTUP_TIME
+# 4.4. Create and get elastic password as well as kibana token
+PASSWORD=$(kubectl -n $NS exec -it $POD -- sh -c "/usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic -b"|grep "New value"|awk '{print $NF}')
+TOKEN=$(kubectl -n $NS exec -it $POD -- sh -c "/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana")
+echo "$PASSWORD"
+echo "$TOKEN"
+
