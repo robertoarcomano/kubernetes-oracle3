87c7646e7ad6b6008a4c9c5a3cb40c6611e9e595
Roberto Arcomano <info@robertoarcomano.com>
2023-03-03 14:41:21 +0100

    Enabled all ns

--- bookstack/update.sh
+++ bookstack/update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # C. Constants
-NS=bookstack
+BOOKSTACK_NS=bookstack-ns.yaml
 DEPLOY_DB=deploy_db.yaml
 SERVICE_DB=service_db.yaml
 DEPLOY_WEB=deploy_web.yaml
 SERVICE_WEB=service_web.yaml
 
-kubectl apply -f $NS
+kubectl apply -f $BOOKSTACK_NS
 kubectl apply -f $DEPLOY_DB
 kubectl apply -f $SERVICE_DB
 kubectl apply -f $DEPLOY_WEB
