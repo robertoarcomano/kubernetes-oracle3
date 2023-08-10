5928211df5d132c37b8c17c44010ac42ebe2f3ba
Roberto Arcomano <info@robertoarcomano.com>
2023-08-10 20:19:19 +0100

    Added KUBECTL to backup

--- backup/update.sh
+++ backup/update.sh
@@ -3,7 +3,8 @@
 # C. Constants
 NS=backup-ns.yaml
 CRONJOB_FILE="cronjob.yaml"
+KUBECTL="microk8s kubectl"
 
-kubectl apply -f $NS
-kubectl apply -f $CRONJOB_FILE
+$KUBECTL apply -f $NS
+$KUBECTL apply -f $CRONJOB_FILE
 
