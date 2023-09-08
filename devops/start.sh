40aa736c6312716a62546fd1f3fcaf1160bde3f7
Roberto Arcomano <info@robertoarcomano.com>
2023-09-08 16:46:46 +0000

    Changed kubectl command

--- devops/start.sh
+++ devops/start.sh
@@ -6,31 +6,32 @@ JENKINS_DEPLOY_FILE=jenkins-deploy.yaml
 JENKINS_POD=""
 JENKINS_SVC_FILE=jenkins-svc.yaml
 PASSWORD_FILE=/var/jenkins_home/secrets/initialAdminPassword
+KUBECTL="microk8s kubectl"
 now="--grace-period=0 --force"
 
 # 0. Reset
-kubectl delete ns $NS $now
+$KUBECTL delete ns $NS $now
 
 # 1. Create ns
-kubectl create ns $NS
+$KUBECTL create ns $NS
 
 # 2. Create deploy jenkins
-kubectl replace -f $JENKINS_DEPLOY_FILE --force
+$KUBECTL replace -f $JENKINS_DEPLOY_FILE --force
 
 # 3. Create service jenkins
-kubectl replace -f $JENKINS_SVC_FILE --force
+$KUBECTL replace -f $JENKINS_SVC_FILE --force
 
 # 4. Create secret
-kubectl create -n $NS secret generic gitlab-registry-auth \
+$KUBECTL create -n $NS secret generic gitlab-registry-auth \
   --from-file=.dockerconfigjson=$HOME/.docker/config.json \
   --type=kubernetes.io/dockerconfigjson
 
 # 5. Wait until pod is ready
-JENKINS_POD=$(kubectl -n $NS get pod --no-headers | grep -i jenkins | awk '{print $1}')
+JENKINS_POD=$($KUBECTL -n $NS get pod --no-headers | grep -i jenkins | awk '{print $1}')
 echo "JENKINS_POD: " $JENKINS_POD
-until kubectl -n $NS get pod $JENKINS_POD|grep Running; do sleep 1; done
-until kubectl -n $NS logs $JENKINS_POD |grep "Jenkins is fully up and running"; do sleep 1; done
+until $KUBECTL -n $NS get pod $JENKINS_POD|grep Running; do sleep 1; done
+until $KUBECTL -n $NS logs $JENKINS_POD |grep "Jenkins is fully up and running"; do sleep 1; done
 
 # 6. Show password
 echo -e "Jenkins password: "
-kubectl -n $NS exec -it $JENKINS_POD -- cat $PASSWORD_FILE
+$KUBECTL -n $NS exec -it $JENKINS_POD -- cat $PASSWORD_FILE
--- website/start.sh
+++ website/start.sh
@@ -7,17 +7,18 @@ DB_DEPLOY_FILE=website-db-deploy.yaml
 DB_SERVICE_FILE=website-db-service.yaml
 WEB_DEPLOY_FILE=website-web-deploy.yaml
 WEB_SERVICE_FILE=website-web-service.yaml
+KUBECTL="microk8s kubectl"
 
 # 0. Re-create namespace
-kubectl delete ns $NS $NOW
-kubectl create ns $NS
+$KUBECTL delete ns $NS $NOW
+$KUBECTL create ns $NS
 
 # 1. Create all resources
-kubectl create -f $DB_DEPLOY_FILE
-kubectl create -f $DB_SERVICE_FILE
-kubectl create -f $WEB_DEPLOY_FILE
-kubectl create -f $WEB_SERVICE_FILE
+$KUBECTL create -f $DB_DEPLOY_FILE
+$KUBECTL create -f $DB_SERVICE_FILE
+$KUBECTL create -f $WEB_DEPLOY_FILE
+$KUBECTL create -f $WEB_SERVICE_FILE
 
 # 2. Show everything
-kubectl get all -n $NS
+$KUBECTL get all -n $NS
 echo
