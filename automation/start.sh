ece7710071e257ccf409833fe719ac266d0db3b6
Roberto Arcomano <info@robertoarcomano.com>
2023-09-25 20:15:56 +0000

    fixing dns issue

--- automation/start.sh
+++ automation/start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # C. Constants
+NS=automation
 AUTOMATION_NS=automation-ns.yaml
 RUNDECK_DEPLOY=rundeck-deploy.yaml
 RUNDECK_SERVICE=rundeck-service.yaml
 now="--grace-period=0 --force"
 
 # 0. Reset
-kubectl delete ns $AUTOMATION_NS $now
+kubectl delete ns $NS $now
 
 # 1. Create ns
-kubectl create ns $AUTOMATION_NS
+kubectl create ns $NS
 
 # 2. Create rundeck deploy
 kubectl apply -f $RUNDECK_DEPLOY
@@ -19,4 +20,4 @@ kubectl apply -f $RUNDECK_DEPLOY
 kubectl apply -f $RUNDECK_SERVICE
 
 # 4. Show everything
-kubectl -n $AUTOMATION_NS get all --show-labels
+kubectl -n $NS get all --show-labels
